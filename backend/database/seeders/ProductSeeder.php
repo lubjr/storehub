0847a3af26cc485b2e232945e9af38f9b74e75c7
lubjr <luizeduardobjr@outlook.com>
2026-03-18 14:19:05 -0300

    fix: use existing categories in productSeeder

--- backend/database/seeders/ProductSeeder.php
+++ backend/database/seeders/ProductSeeder.php
@@ -9,6 +9,10 @@ class ProductSeeder extends Seeder
 {
     public function run(): void
     {
-        Product::factory(50)->create();
+        $categoryIds = \App\Models\Category::pluck('id')->toArray();
+
+        Product::factory(50)->create([
+            'category_id' => fake()->randomElement($categoryIds),
+        ]);
     }
 }
