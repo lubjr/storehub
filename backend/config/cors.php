a80d441d6b6056ceb5b6ec6b644e7262ac5395f9
lubjr <luizeduardobjr@outlook.com>
2026-03-18 15:05:44 -0300

    feat: add ssl https support with lets encrypt

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths'                    => ['api/*'],
     'allowed_methods'          => ['*'],
-    'allowed_origins'          => ['http://localhost:5173', 'http://localhost:3000', 'http://98.92.110.163:3000', 'http://storehub.lubjr.dev'],
+    'allowed_origins'          => ['http://localhost:5173', 'http://localhost:3000', 'http://98.92.110.163:3000', 'http://storehub.lubjr.dev', 'https://storehub.lubjr.dev'],
     'allowed_origins_patterns' => [],
     'allowed_headers'          => ['*'],
     'exposed_headers'          => [],
